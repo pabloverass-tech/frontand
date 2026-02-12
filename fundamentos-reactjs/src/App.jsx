64156928482756b6b7e21f3afad7f37eab8f970d
Pablo Veras <pabloverass@gmail.com>
2026-02-12 00:49:51 -0300

    add commit 12_02_2026

--- fundamentos-reactjs/src/App.jsx
+++ fundamentos-reactjs/src/App.jsx
@@ -1,34 +1,31 @@
-import { Header } from "./components/Header"
-import { Post}  from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
-import './global.css'
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
-
   return (
     <div>
       <Header />
 
-      <div className={styles.wrapper}></div> 
-        <aside>
-          sidebar
-        </aside>
+      <div className={styles.wrapper}>
+        <Sidebar />
         <main>
-           <Post
-          autor="Pablo Veras"
-          content="Lorem psum dolor sit amet consectetur adipisicing elit. Odio, dicta quod. Cum dignissimos quia quibusdam natus, non neque blanditiis ea, adipisci error consectetur consequuntur asperiores et eum modi. Aperiam, dolor."
+          <Post
+            autor="Pablo Veras"
+            content="Lorem psum dolor sit amet consectetur adipisicing elit. Odio, dicta quod. Cum dignissimos quia quibusdam natus, non neque blanditiis ea, adipisci error consectetur consequuntur asperiores et eum modi. Aperiam, dolor."
           />
           <Post
             autor="Pablo Veras 2"
             content="2 Lorem psum dolor sit amet consectetur adipisicing elit. Odio, dicta quod. Cum dignissimos quia quibusdam natus, non neque blanditiis ea, adipisci error consectetur consequuntur asperiores et eum modi. Aperiam, dolor."
           />
-
         </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
